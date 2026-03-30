b5df1bf4da95db3467ff62a10c4594280cfce3c4
Andrey <rybalko102@gmail.com>
2026-03-30 14:18:09 +0300

    update structure

--- Selenium_python/authorization.py
+++ Selenium_python/authorization.py
@@ -8,49 +8,74 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+CONFIG = {
+    'base_url': 'https://www.saucedemo.com/',
+    'window_size': (1920, 1080),
+    'credentials': {
+        'standard_user': {'login': 'standard_user', 'password': 'secret_sauce'}
+    }
+}
+
+
+LOCATORS = {
+    'login_page': {
+        'username': (By.XPATH, "//input[@id='user-name']"),
+        'password': (By.XPATH, "//input[@id='password']"),
+        'login_button': (By.XPATH, "//input[@id='login-button']")
+    }
+}
+
+
 def open_browser(driver): #Открытие сайта указанным браузером
-    try:
-        base_url = 'https://www.saucedemo.com/' # Базовый URL тестового сайта SauceDemo
-        driver.get(base_url) # Переход на указанную страницу в браузере
-        # Установка размера окна браузера в разрешение 1920x1080 (FHD)
-        driver.set_window_size(1920, 1080)
-        time.sleep(1) # Задержка перед выполнением следующей команды
-    except Exception as e:
-        print(f"Ошибка при работе с браузером: {e}") # Вывод ошибки в случае её возникновения
-        driver.close() # Закрытие сайта
+    # Переход на указанную страницу в браузере
+    driver.get(CONFIG.get('base_url'))
+    # Установка размера окна браузера в разрешение 1920x1080 (FHD)
+    driver.set_window_size(*CONFIG.get('window_size'))
 
 
 def close_browser(driver): #Закрытие сайта указанным браузером
-    time.sleep(5) # Задержка перед выполнением следующей команды
-    driver.close() # Закрытие сайта
+    #Закрытие сайта
+    driver.close()
 
 
-def fill_in_the_fields(driver): # Заполнение полей Login и Password
-    # Получение ссылки на элемент (поле) ввода имени пользователя по его локатору
-    user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
-    # Установка значения имени пользователя в поле ввода имени пользователя
-    user_name.send_keys('standard_user')
-    time.sleep(2) # Задержка перед выполнением следующей команды
-    # получение ссылки на элемент (поле) ввода пароля пользователя по его локатору
-    user_password = driver.find_element(By.XPATH, "//input[@id='password']")
-    # Установка значения пароля пользователя в поле ввода пароля
-    user_password.send_keys('secret_sauce')
-    time.sleep(2) # Задержка перед выполнением следующей команды
+def fill_in_the_fields(driver, locator, field_value):
+    # Получение ссылки на элемент (поле) ввода по его локатору
+    user_name = driver.find_element(*locator)
+    # Установка значения в поле
+    user_name.send_keys(field_value)
+    # Задержка перед выполнением следующей команды
+    time.sleep(2)
 
 
-def click_login_button(driver): # Нажатие кнопки Login
+def click_login_button(driver, locator): # Нажатие кнопки Login
     # получение ссылки на кнопку авторизации Login
-    login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
+    login_button = driver.find_element(*locator)
     login_button.click() # Нажатие на кнопку
+    time.sleep(2)
 
 
-def main(): # Основная программа
-    # Получение web драйвера браузера Chrome
+def main(): #Основная программа
+    # получение web драйвера браузера Chrome
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    open_browser(driver) # Открытие сайта
-    fill_in_the_fields(driver) # Заполнение полей
-    click_login_button(driver)  # Нажатие кнопки авторизации (Login)
-    close_browser(driver) # Закрытие браузера
+    open_browser(driver) #Открытие сайта
+    # Заполнение поля логина
+    fill_in_the_fields(
+        driver,
+        LOCATORS.get('login_page')['username'],
+        CONFIG.get('credentials')['standard_user']['login']
+    )
+    # Заполнение поля пароля
+    fill_in_the_fields(
+        driver,
+        LOCATORS.get('login_page')['password'],
+        CONFIG.get('credentials')['standard_user']['password']
+    )
+    # Нажатие на кнопку Login
+    click_login_button(
+        driver,
+        LOCATORS.get('login_page')['login_button']
+    )
+    close_browser(driver) #Закрытие браузера
 
 
 if __name__ == '__main__':
